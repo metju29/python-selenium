3102c2655ea3fec8c6a62034f60a5e50fe2f121b
metju29 <soltysmate@gmail.com>
2022-11-14 22:58:08 +0100

    Modification of the element module in the Page Objects design pattern.

---
+++ selenium_page_object_tutorial/element.py
@@ -0,0 +1,25 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+class BasePageElement(object):
+    """Base page class that is initialized on every page object class."""
+
+    def __set__(self, obj, value):
+        """Sets the text to the value supplied"""
+        driver = obj.driver
+        locator = value[0]
+        text = value[1]
+        WebDriverWait(driver, 100).until(
+            lambda driver: driver.find_element(By.NAME, locator))
+        driver.find_element(By.NAME, locator).clear()
+        driver.find_element(By.NAME, locator).send_keys(text)
+
+    def __get__(self, obj, owner):
+        """Gets the text of the specified object"""
+
+        driver = obj.driver
+        WebDriverWait(driver, 100).until(
+            lambda driver: driver.find_element(By.NAME, self.locator))
+        element = driver.find_element(By.NAME, self.locator)
+        return element.get_attribute("value")
